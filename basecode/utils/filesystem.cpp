2cfde47275ac7a63b95e6a3a73526705af3a352e
Alex Lee <525210804@qq.com>
2018-05-04 09:39:41 +0800

    Update filesystem.cpp
    Added a few functions api for utils package

--- basecode/utils/filesystem.cpp
+++ basecode/utils/filesystem.cpp
@@ -9,3 +9,20 @@ QString Utils::FS::fromNaticePath(const QString &path)
 {
     return QDir::fromNativeSeparators(path);
 }
+
+QString Utils::FS::getFileExtension(const QString &filename)
+{
+    const int pointerIndex = filename.lastIndexOf(".");
+    return ( pointerIndex>= 0)? filename.mid(pointerIndex + 1):QString();
+}
+
+QString Utils::FS::getFileName(const QString &filename)
+{
+    QString name = fromNaticePath(filename);
+    const int slashIndex = name.lastIndexOf("/");
+    if(slashIndex == -1)
+    {
+        return name;
+    }
+    return name.mid(slashIndex + 1);
+}
